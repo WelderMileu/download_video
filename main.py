96a1639a1746e622f2cab393dcb776a265051338
WelderMileu <45273258+WelderMileu@users.noreply.github.com>
2022-10-03 01:26:02 -0300

    Update main.py
    update file

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 #!/usr/bin/python3
 try:
-    import requests
     from sys import argv as param
-    import re
-    import os
-    import uuid
+    import re, os, uuid, requests
 
 except Exception as err:
     print(err)
 
-def main(base_url):
-    try:
-        url           = str(base_url)
-        download_page = "wget -nv {} -O se.html".format(url)
-        os.system(download_page)
+class Simple(base_url):
+    def __init__(self):
+        try:
+            self.url    = str(base_url)
+            self.res    = requests.get(self.url)
+            self.status = self.res.status_code
 
-        r      = requests.get(url)
-        status = r.status_code
+        except Exception as err:
+            print(err)
+
+    def execute(self):
+        try:
+            def download_page_index(download_tool):
+                download_page = "{}".format(download_tool)
+                os.system(download_page)
+
+            if self.status == 200:
+                if os.path.exists('/usr/bin/wget'):
+                    command = "wget -nv {} -O man.html".format(self.url)
+                    download_page_index(command)
+            
+                elif os.path.exists("/usr/bin/curl"):
+                    command = "curl -o man.html {}".format(self.url)
+                    download_page_index(command)
+            
+                else:
+                    print("Please install wget or curl of continue ...")
+                    break
+            
+            else:
+                print("[{}] it'not possible continue status code invalid".format(self.status))
+                break
 
         if status == 200: 
             result = open("se.html", "r")
