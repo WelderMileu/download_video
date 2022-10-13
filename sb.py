e9a94376fa68fa9ce158c7c059e2facfd056bb39
WelderMileu <45273258+WelderMileu@users.noreply.github.com>
2022-10-13 15:16:41 +0000

    Update sb.py
    update code

--- sb.py
+++ sb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import requests
 import os
+from bs4 import BeautifulSoup
 import re
 import json
 
@@ -11,47 +12,40 @@ def main():
         code    = domain.status_code
         content = domain.text
 
-        if code == 200:
-            download_page = "wget -nv {} -O ./se.html".format(url)
-            os.system(download_page)
+        if code == 200: 
+            soup = BeautifulSoup(content)
+            f    = soup.find_all('script')
 
-            read_file = open('./se.html', 'r')
-
-            for x in read_file:
-                if re.search("var stream_data = ", x):
-                    json_d = x[22:-2].replace("'", '"')
-                    # print(json_d)
-
-                    loads = json.loads(json_d)
+            schema = str(f[11])[89:-64].replace("'", '"')
+            loads  = json.loads(schema)
                     
-                    px = ['240p', '320p', '480p', '720p', '1080p', '4k']
-                    px_c = []
+            px = ['240p', '320p', '480p', '720p', '1080p', '4k']
+            px_c = []
 
-                    for t in loads:
-                        # print(loads)
-                        for p in px:
-                            if p == t:
-                                if len(loads[p]) > 0:
-                                    px_c.append(t)
-                                    print("[{}] {}".format(px.index(t), t))
+            for t in loads:
+                for p in px:
+                    if p == t:
+                        if len(loads[p]) > 0:
+                            px_c.append(t)
+                            print("[{}] {}".format(px.index(t), t))
                     
-                    opt = input('Escolha uma das opcoes de download: ')
-                    download = False
+            opt = input('Escolha uma das opcoes de download: ')
+            download = False
 
-                    for p in px_c:
-                        if px[int(opt)] == p:
-                            download = True
+            for p in px_c:
+                if px[int(opt)] == p:
+                    download = True
 
-                    if download: 
-                        domain_s = loads[str(px[int(opt)])]
-                        domain_d = domain_s[0]
-                        
-                        content_length = requests.get(domain_d).headers['Content-Length']
-                        size = float(content_length) / 1024**2
+            if download: 
+                domain_s = loads[str(px[int(opt)])]
+                domain_d = domain_s[0]
+                content_length = requests.get(domain_d).headers['Content-Length']
+                size = float(content_length) / 1024**2
+                    
+                print("Este video tem {:.2f}MB Deseja fazer o download?[y,n]".format(size))
 
-                        print("Este video tem {:.2f}MB Deseja fazer o download?[y,n]".format(size))
-                    else:
-                        print("Opcao invalida")
+            else:
+                print("Opcao invalida")
 
 
         if os.path.exists('./se.html'):
